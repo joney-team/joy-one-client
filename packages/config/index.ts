51d4930d2753b2966139a85dc636e42d5e167a95
jasonvnit <huuthong.mgd@gmail.com>
2025-07-30 13:12:05 +0700

    Change env

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -32,7 +32,7 @@ const environmentConfigs: Record<string, EnvironmentConfig> = {
     PUBLIC_URL: "https://joyone.vn",
     APP_URL: "https://app.joyone.vn",
     API_CLIENT_SIDE_URL: "https://api.joyone.vn",
-    API_SERVER_SIDE_URL: "http://jo-server:4000",
+    API_SERVER_SIDE_URL: "http://joy-one-jo-server-8bogeh:4000",
     SECRET_KEY: "wzsjaledyu",
   },
 }
