301359b80a46b32a0b1fb956a8d4fbf042a98581
jasonvnit <huuthong.mgd@gmail.com>
2025-09-11 22:11:37 +0700

    Fix typescript

--- apps/pwa/src/components/list/columns/enum-column.tsx
+++ apps/pwa/src/components/list/columns/enum-column.tsx
@@ -48,13 +48,14 @@ export const EnumColumn = <T extends string>(args: EnumColumnArgs<T>): Column =>
           value: v.value,
           render: () => {
             const color = useColor();
+            const iconColor = v.color ? color(v.color) : undefined;
 
             return (
               <Group gap={5} w="max-content">
                 {v.icon ? (
-                  <v.icon size={18} color={color(v.color)} />
+                  <v.icon size={18} color={iconColor} />
                 ) : v.color ? (
-                  <Circle color={color(v.color)} size={8} />
+                  <Circle color={iconColor} size={8} />
                 ) : null}
 
                 <Text fz={14} fw={500}>
