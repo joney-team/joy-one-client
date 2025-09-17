61c88f7ae4a1f6520c1c2bdd42a53761912ae886
jasonvnit <huuthong.mgd@gmail.com>
2025-09-17 08:33:07 +0700

    update

--- apps/pwa/src/modules/orders/orders-sale/components/order-sale-actions.tsx
+++ apps/pwa/src/modules/orders/orders-sale/components/order-sale-actions.tsx
@@ -4,7 +4,7 @@ import { useLayout } from "@/layout/layout-context";
 import { t } from "@/modules/lang/lang-service";
 import { ActionIcon, Group, Menu } from "@mantine/core";
 import { IconCirclePlus, IconDots, IconPrinter, IconTrash } from "@tabler/icons-react";
-import { type FC } from "react";
+import { Fragment, type FC } from "react";
 import { userOrdersManagement } from "../../orders-management/orders-management-context";
 
 export const OrderSaleActions: FC = () => {
@@ -18,26 +18,28 @@ export const OrderSaleActions: FC = () => {
           <IconCirclePlus size={20} strokeWidth={1.5} />
         </ActionIcon>
 
-        <Menu>
-          <Menu.Target>
-            <ActionIcon>
-              <IconDots size={20} strokeWidth={1.5} />
-            </ActionIcon>
-          </Menu.Target>
+        {orderSale.activeOrder && (
+          <Menu>
+            <Menu.Target>
+              <ActionIcon>
+                <IconDots size={20} strokeWidth={1.5} />
+              </ActionIcon>
+            </Menu.Target>
 
-          <Menu.Dropdown>
-            <Menu.Item
-              onClick={orderSale.removeOrder}
-              leftSection={<IconTrash size={20} strokeWidth={1.5} />}
-            >
-              {t("archive")}
-            </Menu.Item>
+            <Menu.Dropdown>
+              <Menu.Item
+                onClick={orderSale.removeOrder}
+                leftSection={<IconTrash size={20} strokeWidth={1.5} />}
+              >
+                {t("archive")}
+              </Menu.Item>
 
-            <Menu.Item leftSection={<IconPrinter size={20} strokeWidth={1.5} />}>
-              {t("print")}
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
+              <Menu.Item leftSection={<IconPrinter size={20} strokeWidth={1.5} />}>
+                {t("print")}
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        )}
       </Group>
     );
   }
@@ -48,13 +50,17 @@ export const OrderSaleActions: FC = () => {
         <IconCirclePlus size={20} strokeWidth={1.5} />
       </ActionIcon>
 
-      <ActionIcon>
-        <IconPrinter size={20} strokeWidth={1.5} />
-      </ActionIcon>
+      {orderSale.activeOrder && (
+        <Fragment>
+          <ActionIcon>
+            <IconPrinter size={20} strokeWidth={1.5} />
+          </ActionIcon>
 
-      <ActionIcon onClick={orderSale.removeOrder}>
-        <IconTrash size={20} strokeWidth={1.5} />
-      </ActionIcon>
+          <ActionIcon onClick={orderSale.removeOrder}>
+            <IconTrash size={20} strokeWidth={1.5} />
+          </ActionIcon>
+        </Fragment>
+      )}
     </Group>
   );
 };
