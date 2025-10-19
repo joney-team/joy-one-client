f7b62b6efe5faa99372cba6f5d7c6c7796783af2
jasonvnit <huuthong.mgd@gmail.com>
2025-10-19 08:52:03 +0700

    update

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -8,6 +8,7 @@ export interface EnvironmentConfig {
   API_SERVER_SIDE_URL: string;
   SECRET_KEY: string;
   ANALYTICS_KEY: string;
+  SENTRY_DSN: string;
 }
 
 const environmentConfigs: Record<string, EnvironmentConfig> = {
@@ -19,6 +20,7 @@ const environmentConfigs: Record<string, EnvironmentConfig> = {
     API_SERVER_SIDE_URL: "http://localhost:4000",
     SECRET_KEY: "wzsjaledyu",
     ANALYTICS_KEY: "t9j567r7qx",
+    SENTRY_DSN: "https://73c47d7433184a648c959aed0875411a@glitchtip.joyone.vn/2",
   },
   staging: {
     PUBLIC_URL: "https://staging.joyone.vn",
@@ -28,6 +30,7 @@ const environmentConfigs: Record<string, EnvironmentConfig> = {
     API_SERVER_SIDE_URL: "http://joy-one-staging-server-apis-ol2vcf:4000",
     SECRET_KEY: "wzsjaledyu",
     ANALYTICS_KEY: "t9j567r7qx",
+    SENTRY_DSN: "https://73c47d7433184a648c959aed0875411a@glitchtip.joyone.vn/2",
   },
   production: {
     PUBLIC_URL: "https://joyone.vn",
@@ -37,6 +40,7 @@ const environmentConfigs: Record<string, EnvironmentConfig> = {
     API_SERVER_SIDE_URL: "http://joy-one-server-apis-jvh0gk:4000",
     SECRET_KEY: "wzsjaledyu",
     ANALYTICS_KEY: "t9j567r7qx",
+    SENTRY_DSN: "https://73c47d7433184a648c959aed0875411a@glitchtip.joyone.vn/2",
   },
 };
 
