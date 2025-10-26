6ee2409bea4a4a35960f41d584e7715c497e4c36
jasonvnit <huuthong.mgd@gmail.com>
2025-10-26 10:49:46 +0700

    update

--- apps/pwa/src/layout/header/header-shortcuts.tsx
+++ apps/pwa/src/layout/header/header-shortcuts.tsx
@@ -148,7 +148,7 @@ export const WorkspaceHeaderShortcuts: FC = memo(() => {
           <Menu.Target>
             <Group>
               <Button id="create-credit" size="xs" leftIcon={IconCirclePlus} isGradient>
-                {t`Quick action`}
+                {t`Quick create`}
               </Button>
             </Group>
           </Menu.Target>
