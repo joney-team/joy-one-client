1b3fcb022fb971cc206070a602482aa4d44c4f5e
jasonvnit <huuthong.mgd@gmail.com>
2025-11-09 00:40:23 +0700

    update

--- apps/website/src/lang/hooks.ts
+++ apps/website/src/lang/hooks.ts
@@ -1,10 +1,13 @@
 import { useRouter } from "next/router";
 import { Locale } from "./types";
+import { dictionaries } from "./dictionary";
+import ReactHtmlParser from "html-react-parser";
 
 export interface UseLang {
   locale: Locale;
   num: (value: number) => string;
   switchLocale: (locale?: Locale) => void;
+  t: (key: string) => string;
 }
 
 export const localeNames = {
@@ -26,6 +29,22 @@ export const useLang = (): UseLang => {
         router.push({ pathname: router.pathname, query: router.query }, undefined, { locale });
       } else {
         router.push({ pathname: router.pathname, query: router.query });
+      }
+    },
+    t: (key: string, params?: any) => {
+      try {
+        let sentence = dictionaries[key][_locale] || dictionaries[key][Locale.EN];
+
+        if (params) {
+          Object.keys(params).forEach((param) => {
+            sentence = sentence.replace(`{${param}}`, params[param]);
+          });
+        }
+
+        if (/<\/?[a-z][\s\S]*>/.test(sentence)) return ReactHtmlParser(sentence) as string;
+        return sentence;
+      } catch (error) {
+        return key;
       }
     },
   };
