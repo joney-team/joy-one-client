e1b1c761316a3cb074bd463424460781f6ac14e4
jasonvnit <huuthong.mgd@gmail.com>
2025-11-09 12:43:48 +0700

    update

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,18 +1,13 @@
-import { parse } from "dotenv";
-
-const env = parse(".env");
-
 const defaultConfig = {
-  ENV: env.ENV,
-  PUBLIC_URL: env.PUBLIC_URL,
-  APP_URL: env.APP_URL,
-  API_TOOLS_URL: env.API_TOOLS_URL,
-  API_CLIENT_SIDE_URL: env.API_CLIENT_SIDE_URL,
-  API_SERVER_SIDE_URL: env.API_SERVER_SIDE_URL,
-  SECRET_KEY: env.SECRET_KEY,
-  ANALYTICS_KEY: env.ANALYTICS_KEY,
-  SENTRY_DSN: env.SENTRY_DSN,
-  IS_DEVELOPMENT: env.ENV === "development",
+  ENV: "development",
+  PUBLIC_URL: "http://localhost:3000",
+  APP_URL: "http://localhost:3000",
+  API_TOOLS_URL: "http://localhost:4000",
+  API_CLIENT_SIDE_URL: "http://localhost:4000",
+  API_SERVER_SIDE_URL: "http://localhost:4000",
+  SECRET_KEY: "wzsjaledyu",
+  ANALYTICS_KEY: "",
+  SENTRY_DSN: "",
 };
 
 export type Config = typeof defaultConfig;
