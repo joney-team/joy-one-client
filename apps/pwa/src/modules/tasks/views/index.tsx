9681810f5b73337140ff52401a7edd68043da3c2
jasonvnit <huuthong.mgd@gmail.com>
2025-12-01 18:58:01 +0700

    update

--- apps/pwa/src/modules/tasks/views/index.tsx
+++ apps/pwa/src/modules/tasks/views/index.tsx
@@ -3,7 +3,7 @@
 import { useTasks } from "@/modules/tasks/tasks-context";
 import { nonLoading } from "@/utils/non-loading";
 import { Trans } from "@lingui/react/macro";
-import { Skeleton, Stack } from "@mantine/core";
+import { Skeleton } from "@mantine/core";
 import {
   Icon,
   IconCalendar,
@@ -14,16 +14,7 @@ import {
 } from "@tabler/icons-react";
 import dynamic from "next/dynamic";
 import { useParams, useRouter } from "next/navigation";
-import {
-  ComponentType,
-  FC,
-  Fragment,
-  PropsWithChildren,
-  ReactNode,
-  Suspense,
-  useEffect,
-  useMemo,
-} from "react";
+import { ComponentType, FC, Fragment, PropsWithChildren, ReactNode, useEffect } from "react";
 import { BulkTasksActions } from "../components/bulk-tasks-actions";
 import { TaskView } from "./types";
 
@@ -48,42 +39,13 @@ const TasksRealtimeEvents = dynamic(
   }
 );
 
-const viewLoader = () => (
-  <Stack p={16}>
-    <Skeleton height={500} />
-  </Stack>
+const TaskViewComponent = dynamic(
+  () => import("./task-view").then((mod) => mod.TaskViewComponent),
+  {
+    ssr: false,
+    loading: nonLoading,
+  }
 );
-
-// Lazy load factory functions - only create dynamic imports when needed
-const getListTasks = () =>
-  dynamic(() => import("./list/list-tasks").then((mod) => mod.ListTasks), {
-    ssr: false,
-    loading: viewLoader,
-  });
-
-const getBoardTasks = () =>
-  dynamic(() => import("./board/board-tasks").then((mod) => mod.TasksBoardView), {
-    ssr: false,
-    loading: viewLoader,
-  });
-
-const getGanttTasks = () =>
-  dynamic(() => import("./gantt").then((mod) => mod.TasksGantt), {
-    ssr: false,
-    loading: viewLoader,
-  });
-
-const getTimeTrackingsTasks = () =>
-  dynamic(() => import("./time-trackings").then((mod) => mod.TasksTimeTrackings), {
-    ssr: false,
-    loading: viewLoader,
-  });
-
-const getCalendarTasks = () =>
-  dynamic(() => import("./calendar").then((mod) => mod.TasksCalendarView), {
-    ssr: false,
-    loading: viewLoader,
-  });
 
 const allTaskViews: {
   [key in TaskView]: {
@@ -97,27 +59,22 @@ const allTaskViews: {
   [TaskView.LIST]: {
     icon: IconList,
     name: <Trans>List</Trans>,
-    loader: getListTasks,
   },
   [TaskView.BOARD]: {
     icon: IconLayoutKanban,
     name: <Trans>Board</Trans>,
-    loader: getBoardTasks,
   },
   [TaskView.GANTT]: {
     icon: IconMist,
     name: <Trans>Gantt</Trans>,
-    loader: getGanttTasks,
   },
   [TaskView.TIME_TRACKINGS]: {
     icon: IconStopwatch,
     name: <Trans>Time trackings</Trans>,
-    loader: getTimeTrackingsTasks,
   },
   [TaskView.CALENDAR]: {
     icon: IconCalendar,
     name: <Trans>Calendar</Trans>,
-    loader: getCalendarTasks,
   },
 };
 
@@ -139,18 +96,6 @@ const TasksViews: FC<PropsWithChildren> = (props) => {
     }
   }, [isReady, params, view, activatedFolder?.slug, router]);
 
-  // Lazy load component only when view is active, with caching
-  const ViewComponent = useMemo(() => {
-    const viewConfig = allTaskViews[view];
-    if (!viewConfig?.loader) {
-      return null;
-    }
-
-    // Load and cache the component
-    const Component = viewConfig.loader();
-    return Component;
-  }, [view]);
-
   return (
     <Fragment>
       <NavigationTabs
@@ -165,12 +110,7 @@ const TasksViews: FC<PropsWithChildren> = (props) => {
 
       {props.children}
 
-      {ViewComponent && (
-        <Suspense fallback={<Skeleton height={300} />}>
-          <ViewComponent />
-        </Suspense>
-      )}
-
+      <TaskViewComponent view={view} />
       <TasksRealtimeEvents />
       <TaskDetail />
       <BulkTasksActions />
---
+++ apps/pwa/src/modules/tasks/views/task-view.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Skeleton, Stack } from "@mantine/core";
+import dynamic from "next/dynamic";
+import { ComponentType, ReactNode, useMemo, useRef, type FC } from "react";
+import { TaskView } from "./types";
+
+const viewLoader = () => (
+  <Stack p={16}>
+    <Skeleton height={500} />
+  </Stack>
+);
+
+const getListTasks = () =>
+  dynamic(() => import("./list/list-tasks").then((mod) => mod.ListTasks), {
+    ssr: false,
+    loading: viewLoader,
+  });
+
+const getBoardTasks = () =>
+  dynamic(() => import("./board/board-tasks").then((mod) => mod.TasksBoardView), {
+    ssr: false,
+    loading: viewLoader,
+  });
+
+const getGanttTasks = () =>
+  dynamic(() => import("./gantt").then((mod) => mod.TasksGantt), {
+    ssr: false,
+    loading: viewLoader,
+  });
+
+const getTimeTrackingsTasks = () =>
+  dynamic(() => import("./time-trackings").then((mod) => mod.TasksTimeTrackings), {
+    ssr: false,
+    loading: viewLoader,
+  });
+
+const getCalendarTasks = () =>
+  dynamic(() => import("./calendar").then((mod) => mod.TasksCalendarView), {
+    ssr: false,
+    loading: viewLoader,
+  });
+
+const allTaskViews: {
+  [key in TaskView]: {
+    loader?: () => ComponentType<{ children?: ReactNode | undefined }>;
+  };
+} = {
+  [TaskView.LIST]: { loader: getListTasks },
+  [TaskView.BOARD]: { loader: getBoardTasks },
+  [TaskView.GANTT]: { loader: getGanttTasks },
+  [TaskView.TIME_TRACKINGS]: { loader: getTimeTrackingsTasks },
+  [TaskView.CALENDAR]: { loader: getCalendarTasks },
+};
+
+export const TaskViewComponent: FC<{ view: TaskView }> = ({ view }) => {
+  const cachedComponents = useRef<
+    Partial<Record<TaskView, ComponentType<{ children?: ReactNode | undefined }>>>
+  >({});
+
+  // Lazy load component only when view is active, with caching
+  const ViewComponent = useMemo(() => {
+    const viewConfig = allTaskViews[view];
+    if (!viewConfig?.loader) {
+      return null;
+    }
+
+    if (cachedComponents.current[view]) {
+      return cachedComponents.current[view];
+    }
+
+    // Load and cache the component
+    const Component = viewConfig.loader();
+    cachedComponents.current[view] = Component;
+    return Component;
+  }, [view]);
+
+  if (!ViewComponent) {
+    return null;
+  }
+
+  return <ViewComponent />;
+};
