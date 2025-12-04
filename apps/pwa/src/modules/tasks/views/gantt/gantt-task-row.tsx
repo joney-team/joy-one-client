7bd7ab38a18053ce98b77b28803079e7fe82e901
jasonvnit <huuthong.mgd@gmail.com>
2025-12-05 00:54:26 +0700

    update

--- apps/pwa/src/modules/tasks/views/gantt/gantt-task-row.tsx
+++ apps/pwa/src/modules/tasks/views/gantt/gantt-task-row.tsx
@@ -497,6 +497,7 @@ export const GanttTaskRow: FC<GanttTaskRowProps> = ({
             variables: groupVariables,
           },
         ],
+        awaitRefetchQueries: true,
       });
     } catch (error) {
       onError(error);
