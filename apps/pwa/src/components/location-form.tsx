fbfb59a56082ed77325aa127807e8cd9c15cd9b2
jasonvnit <huuthong.mgd@gmail.com>
2026-03-28 12:38:29 +0700

    update

--- apps/pwa/src/components/location-form.tsx
+++ apps/pwa/src/components/location-form.tsx
@@ -7,15 +7,18 @@ import { useLocations } from "@/modules/locations/locations-context";
 import { optionsFilter } from "@/modules/theme/generate-theme";
 import { Trans } from "@lingui/react/macro";
 import { Select, TextInput } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
 
 export const LocationForm: FC<{
-  form: any;
+  form: UseFormReturnType<any>;
   path?: string;
   required?: boolean;
 }> = memo((props) => {
   const { form } = props;
   const { vnLocations } = useLocations();
   const path = props.path || "vnLocation";
+
+  const input = form.getInputProps(path);
 
   return (
     <Fragment>
@@ -28,12 +31,16 @@ export const LocationForm: FC<{
             .filter((l) => l.type === "province")
             .map((l) => ({ value: l.id, label: l.name }))}
           onChange={(e) => {
-            form.setFieldValue(`${path}.provinceId`, e!);
-            form.setFieldValue(`${path}.wardId`, "");
+            form.setFieldValue(path, {
+              ...(input.value ?? {}),
+              provinceId: e,
+              wardId: "",
+            });
           }}
           filter={optionsFilter}
           withAsterisk={props.required}
         />
+
         <Select
           label={<Trans>Ward</Trans>}
           {...form.getInputProps(`${path}.wardId`)}
@@ -43,7 +50,7 @@ export const LocationForm: FC<{
               (l) =>
                 l.type === "ward" &&
                 l.parentId === form.values[path]?.provinceId &&
-                form.values[path]?.provinceId
+                form.values[path]?.provinceId,
             )
             .map((l) => ({ value: l.id, label: l.fullName }))}
           flex={1}
@@ -55,6 +62,12 @@ export const LocationForm: FC<{
       <TextInput
         label={<Trans>Address</Trans>}
         {...form.getInputProps(`${path}.address`)}
+        onChange={(e) => {
+          form.setFieldValue(path, {
+            ...(input.value ?? {}),
+            address: e.currentTarget.value,
+          });
+        }}
         withAsterisk={props.required}
       />
     </Fragment>
