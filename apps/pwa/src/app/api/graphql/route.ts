0e54f6b916c41659b8115dadc2eb9019cdbb6d95
jasonvnit <huuthong.mgd@gmail.com>
2026-04-05 00:28:38 +0700

    update

--- apps/pwa/src/app/api/graphql/route.ts
+++ apps/pwa/src/app/api/graphql/route.ts
@@ -4,7 +4,7 @@ import { StorageKey } from "@/constants/storage-key";
 import config from "@joy-one-client/config";
 import { cookies } from "next/headers";
 
-const acceptHeaders = ["x-device-id", "x-session-id", "x-workspace-id", "accept-language"];
+const forwardHeaders = ["x-device-id", "x-session-id", "x-workspace-id", "accept-language"];
 
 export async function POST(req: Request) {
   const [body, cookieStore] = await Promise.all([req.text(), cookies()]);
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
   const response = await fetch(config.API_SERVER_SIDE_URL + "/graphql", {
     method: "POST",
     headers: {
-      ...Object.fromEntries(acceptHeaders.map((header) => [header, req.headers.get(header)])),
+      ...Object.fromEntries(forwardHeaders.map((header) => [header, req.headers.get(header)])),
       "Content-Type": "application/json",
       Authorization: `Bearer ${cookieStore.get(StorageKey.ACCESS_TOKEN)?.value}`,
     },
